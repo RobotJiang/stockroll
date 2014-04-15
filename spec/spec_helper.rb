0e8cdd90100a163514b674ab444365965d25fa65
Robot Jiang <robot.z.jiang@gmail.com>
2014-04-15 14:48:18 +0800

    remove coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,7 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require "stockroll"
-require 'coveralls'
 
-Coveralls.wear!
 
 RSpec.configure do |config|
   # Use color in STDOUT
